2cf6887a7d0ba763f5af65a48582be3c05bcb1b7
Aycan Özyılmaz <aycanozyilmaz@gmail.com>
2020-07-14 10:42:41 +0300

    Add project files.

---
+++ Models/Konut.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace V3.Models
+{
+    public class Konut : INotifyPropertyChanged
+    {
+        public int Alan
+        {
+            get; set;
+        }
+
+        public int Fiyat
+        {
+            get; set;
+        }
+
+        public int bahceAlan
+        {
+            get; set;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using V3.Models;
+
+namespace V3.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public ObservableCollection<Konut> Konutlar { get; set; }
+
+        public HomeViewModel()
+        {
+            Konutlar = new ObservableCollection<Konut>
+            {
+                new Konut{Alan = 500, Fiyat = 800, bahceAlan = 200},
+                new Konut{Alan = 400, Fiyat = 750, bahceAlan = 100},
+                new Konut{Alan = 450, Fiyat = 600, bahceAlan = 150},
+                new Konut{Alan = 600, Fiyat = 900, bahceAlan = 300}
+            };
+        }
+
+    }
+}
